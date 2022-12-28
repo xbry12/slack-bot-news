6712a4e64fa5be2fd9a1863c60cab0ccf5b722da
Pyram, Bryant <BPyram@@cvent.com>
2022-12-28 16:18:16 -0500

    added functionality and dynamics to app

--- app.js
+++ app.js
@@ -9,18 +9,37 @@ const app = new App({
 });
 
 // Listens to incoming messages that contain "hello"
-app.command('/news', async ({ command, ack, say }) => {
 
+// The echo command simply echoes on command
+app.command('/echo', async ({ command, ack, respond }) => {
+    // Acknowledge command request
+    await ack();
+
+    await respond(`${command.text}`);
+});
+
+app.command('/news', async ({ command, ack, say }) => {
+    console.log(command);
     await ack()
     // say() sends a message to the channel where the event was triggered
-    var url = 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=dcd8f6cdbfa344d7baa998e7ad19d61c'
+    var url = `https://newsapi.org/v2/top-headlines?country=us&category=${command.text}&apiKey=dcd8f6cdbfa344d7baa998e7ad19d61c`
+    // console.log(url)
+    // if user enters news 
+    // return error Usage case etc..
+
+    if (command.text == '') {
+        await say('Please include one of the following categories: Business, Entertainment, General, Health, Science, Sports, Technology');
+        return;
+    }
+
+
 
     const json_response = await fetch(url).then((response) => {
         return response.json();
-    }).catch((err) => {
+    }).catch((err) => {m
         console.log('rejected', err);
     });
-
+    // console.log(json_response);
     var articles_array = []
 
     for (let i = 0; i < 5; i++) {
@@ -49,6 +68,7 @@ app.command('/news', async ({ command, ack, say }) => {
     await say({
         blocks: articles_array
     });
+    // await respond(`${command.text}`);
 });
 
 (async () => {
